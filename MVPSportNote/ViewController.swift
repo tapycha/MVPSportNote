be019928180ee9e686a68df9b1bc700300fa9198
DoctorApp <vladyslav.gamalii@nerdpeople.com>
2020-05-19 11:16:51 -0700

    Initial Commit

---
+++ MVPSportNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVPSportNote
+//
+//  Created by Andrew Peneznyk on 19.05.2020.
+//  Copyright © 2020 Andrew Penzenyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
